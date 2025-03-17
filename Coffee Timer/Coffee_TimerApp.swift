31096de9f88e449dc46f17eb3cfaf5d5518c63e1
Harrison Fornasier <harrisonfornasier@gmail.com>
2025-03-17 10:32:06 +0000

    Initial Commit

---
+++ Coffee Timer/Coffee_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_TimerApp.swift
+//  Coffee Timer
+//
+//  Created by Harrison Fornasier on 17/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Coffee TimerTests/Coffee_TimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_TimerTests.swift
+//  Coffee TimerTests
+//
+//  Created by Harrison Fornasier on 17/03/2025.
+//
+
+import Testing
+@testable import Coffee_Timer
+
+struct Coffee_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
